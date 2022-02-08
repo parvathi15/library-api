9d7841b615c963cf57469247ab038b2b869e103f
parvathi15 <menonparvathi23@gmail.com>
2022-02-08 17:03:36 +0100

    new files

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
+//packages
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose"); //connecting to mongodb database
 
-app.get("/", function (req, res) {
-  res.send("WORKING!!!");
+//environment variables in .env file
+require("dotenv").config();
+
+//express server
+const app = express();
+const port = process.env.PORT || 2000;
+
+//cors middleware for sending and receiving json
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+const uri = process.env.ATLAS_URI; //mondodbatlas dashboard database
+mongoose.connect(uri, { useNewUrlParser: true }); //flags
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
-app.listen(process.env.PORT || 1500);
+//connection to routes
+const BooksRouter = require("./routes/book.js");
+// const membersRouter = require("./routes/member.js");
+const membersRouter = require("./routes/members.js");
+const requestsRouter = require("./routes/request.js");
+
+
+app.use("/members", membersRouter);
+
+app.use("/books", BooksRouter);
+app.use("/requests", requestsRouter);
+
+app.get("/requests", function(req,res){
+  var queryparameter = req.query;
+  console.log(queryparameter.status)
+  res.json(queryparameter)
+})
+// app.use("/members", membersRouter);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("WORKING!!!");
+// });
+
+// app.listen(process.env.PORT || 1500);
